f32a51a4817af2de4bb889b04ecb784550d4ce5e
Abdelrhman Reda <abdelrhmanreda818@gmail.com>
2026-04-24 15:34:58 +0300

    Add files via upload

---
+++ Day3/oop task1/oop task1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace oop_task1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Student ahmed = new Student("Ahmed" , 23 , 89);
+            ahmed.DisplayInfo();
+            ahmed.setGrade(105);
+            ahmed.DisplayInfo();
+        }
+    }
+}
---
+++ Day3/oop task1/oop task1/Student.cs
@@ -0,0 +1,49 @@
+﻿class Student
+{
+    private string name;
+    private int age;
+    private int grade;
+
+    public Student(string Name, int Age, int Grade)
+    {
+        setName(Name);
+        setAge(Age);
+        setGrade(Grade);
+    }
+
+    public void setName(string Name)
+    {
+        name = Name;
+    }
+
+    public void setAge(int Age)
+    {
+        while (Age < 6)
+        {
+            Console.WriteLine("Age must be greater than 5");
+            Age = int.Parse(Console.ReadLine());
+        }
+        age = Age;
+    }
+
+    public void setGrade(int Grade)
+    {
+        while (Grade < 0 || Grade > 100)
+        {
+            Console.WriteLine("Grade must be between 0 and 100");
+            Grade = int.Parse(Console.ReadLine());
+        }
+        grade = Grade;
+    }
+
+    public string getName() => name;
+    public int getAge() => age;
+    public int getGrade() => grade;
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Student name is {name}");
+        Console.WriteLine($"Student age is {age}");
+        Console.WriteLine($"Student grade is {grade}");
+    }
+}
