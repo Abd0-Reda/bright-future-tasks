8f4caa2d1561fb59a10d31e735ec02ef2bec6cad
Abdelrhman Reda <abdelrhmanreda818@gmail.com>
2026-05-02 16:53:04 +0300

    Add files via upload

---
+++ Day7/Advancedc#/Advancedc#/Hero_Squad_Manager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advancedc_
+{
+    class Hero
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+    }
+
+    class Squad<T> : IEnumerable<T>
+    {
+        private List<T> elements = new List<T>();
+
+        public void Add(T item)
+        {
+            elements.Add(item);
+        }
+
+        public List<Hero> FindStrongHeroes()
+        {
+            List<Hero> result = new List<Hero>();
+
+            foreach (var item in elements)
+            {
+                if (item is Hero h && h.Level >= 50)
+                {
+                    result.Add(h);
+                }
+            }
+
+            return result;
+        }
+
+        public T Find(Func<T, bool> condition)
+        {
+            foreach (var item in elements)
+            {
+                if (condition(item))
+                {
+                    return item;
+                }
+            }
+            return default;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return elements.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
---
+++ Day7/Advancedc#/Advancedc#/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Advancedc_;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Squad<Hero> squad = new Squad<Hero>();
+
+        squad.Add(new Hero { Name = "Batman", Level = 40 });
+        squad.Add(new Hero { Name = "Dr.Strange", Level = 100 });
+        squad.Add(new Hero { Name = "Superman", Level = 80 });
+
+        var strongHeroes = squad.FindStrongHeroes();
+
+        Console.WriteLine("Strong Heroes:");
+        foreach (var hero in strongHeroes)
+        {
+            Console.WriteLine(hero.Name);
+        }
+
+        var batman = squad.Find(delegate (Hero h)
+        {
+            return h.Name == "Batman";
+        });
+
+        Console.WriteLine("Search Result:" + batman.Name);
+
+    }
+}
